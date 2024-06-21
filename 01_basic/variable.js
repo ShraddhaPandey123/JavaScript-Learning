b7f192f3b35acba2ce57ba5995896b351671ec96
Shraddha Pandey <140546155+ShraddhaPandey123@users.noreply.github.com>
2024-06-21 10:28:09 +0000

    var let const difference

---
+++ 01_basic/variable.js
@@ -0,0 +1,22 @@
+const accountId=12345
+let accountEmail="shraddhapandey1259@gmail.com"
+var accountPassword=5236
+accountCity="Noida Electronic City"
+let accountState;
+
+// accountId=52 
+// It will throw an error i.e; Assignment to constant variable 
+console.log(accountId);
+
+
+
+accountEmail = "shraddha1234@gmail.com"
+console.log(accountEmail);
+
+accountPassword = "789654"
+console.log(accountPassword);
+
+accountCity="Delhi"
+console.log(accountCity);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
