dbbbf33f6c8262577bdb321bbae4dd2c0ecb1828
Shraddha Pandey <140546155+ShraddhaPandey123@users.noreply.github.com>
2024-06-25 05:03:46 +0000

    operators done

---
+++ 01_basic/operator.js
@@ -0,0 +1,50 @@
+//Arithmetic Operator
+
+/*console.log(2+2); // addition
+console.log(2-2); // subtraction
+console.log(2*2); // multiplication
+console.log(2**2); //power
+console.log(2/3); // divide
+console.log(2%3); //modulus */
+
+
+
+//string concatenation => generate same output
+/* let str1="shraddha";
+let str2="pandey" ;
+let str3=str1+str2;
+console.log(str3);
+
+
+let str1="shraddha";
+let str2="pandey" ;
+let str3=str1+""+str2;
+console.log(str3);
+*/
+
+
+
+/*let str1="shraddha";
+let str2=" pandey" ;
+let str3=str1+str2;
+console.log(str3);
+
+
+let str1="shraddha";
+let str2="pandey" ;
+let str3=str1+" "+str2;
+console.log(str3); */
+
+
+let str1=("1"+"2");
+console.log(str1);  //12
+let str2=("1"+2);
+console.log(str2);  //12
+let str3=(1+"2");
+console.log(str3);  //12
+let str4=("1"+2+3);
+console.log(str4);  //123
+let str5=(1+2+3+"5");
+console.log(str5);  //65
+
+
