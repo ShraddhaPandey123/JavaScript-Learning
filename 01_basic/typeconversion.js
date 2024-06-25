d94e58c7034599ec3426d5d1265b31690e107b9e
Shraddha Pandey <140546155+ShraddhaPandey123@users.noreply.github.com>
2024-06-25 04:29:57 +0000

    typeconversion done

---
+++ 01_basic/typeconversion.js
@@ -0,0 +1,131 @@
+// let number=56;
+/*About typeof => typeof is a operator returns the data type of a javascript variable.
+console.log(number);
+console.log(typeof number);
+console.log(typeof (number));*/
+
+
+
+// conversion from number to number
+/*let someNumber=78;
+let valueOfSomeNumber=Number(someNumber);
+console.log(valueOfSomeNumber);
+console.log(typeof valueOfSomeNumber);*/
+
+
+
+
+// conversion from number to string
+/* let someNumber=78;
+let valueOfSomeNumber=String(someNumber);
+console.log(valueOfSomeNumber);
+console.log(typeof valueOfSomeNumber);*/
+
+
+
+// conversion from number as undefined to string
+/*let number=undefined;
+let valueOfnumber=String(number);
+console.log(valueOfnumber);
+console.log(typeof valueOfnumber);*/
+
+
+// conversion from number as undefined to boolean
+/*let number=undefined;
+let valueOfnumber=Boolean(number);
+console.log(valueOfnumber);
+console.log(typeof valueOfnumber);*/
+
+
+
+// conversion from number as undefined to number
+/*let number=undefined;
+let valueOfnumber=Number(number);
+console.log(valueOfnumber);
+console.log(typeof valueOfnumber);*/
+
+
+
+// conversion from string to string
+/*let someString="shraddha";
+let valueOfSomeString=String(someString);
+console.log(valueOfSomeString);
+console.log(typeof valueOfSomeString);*/
+
+
+
+
+// conversion from string  to number
+/*let someString="shraddha";
+let valueOfSomeString=Number(someString);
+console.log(valueOfSomeString); 
+console.log(typeof valueOfSomeString);*/
+
+
+// conversion from string  to Boolean
+/*let someString="";
+let valueOfSomeString=Boolean(someString);
+console.log(valueOfSomeString); 
+console.log(typeof valueOfSomeString);*/
+
+
+
+// conversion from string  to Boolean
+/*let someString="shraddha";
+let valueOfSomeString=Boolean(someString);
+console.log(valueOfSomeString); 
+console.log(typeof valueOfSomeString);*/
+
+
+// conversion from null to boolean
+/*let temperature=null;
+let valueOfTemperature=Boolean(temperature);
+console.log(valueOfTemperature); 
+console.log(typeof valueOfTemperature);*/
+
+
+
+// conversion from undefined to boolean
+/*let temperature=undefined;
+let valueOfTemperature=Boolean(temperature);
+console.log(valueOfTemperature); 
+console.log(typeof valueOfTemperature);*/
+
+
+
+
+// conversion from undefined to boolean
+let number=0;
+let booleanValue=Boolean(number);
+console.log(booleanValue);
+console.log(typeof booleanValue);
+
+
+
+// conversion from undefined to boolean
+/*let isLoggedIn = 1;
+let booleanValue=Boolean(isLoggedIn);
+console.log(booleanValue);
+console.log(typeof booleanValue);*/
+
+
+
+// conversion from boolean to number
+/*let isLoggedIn = true;
+let booleanValue=Number(isLoggedIn);
+console.log(booleanValue);
+console.log(typeof booleanValue);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
